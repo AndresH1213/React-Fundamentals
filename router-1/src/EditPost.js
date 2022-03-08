4720f91a7bc18470dbc5253fb72ea4d84200fcd9
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-08 17:37:34 -0500

    Data retrieving from each component where it belong

--- router-1/src/EditPost.js
+++ router-1/src/EditPost.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import React, { useEffect, useContext, useState } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import DataContext from "./context/DataContext";
+import { format } from "date-fns";
+import api from "./api/posts";
 
-const EditPost = ({
-  posts,
-  handleEdit,
-  editBody,
-  setEditBody,
-  editTitle,
-  setEditTitle,
-}) => {
+const EditPost = () => {
+  const { posts, setPosts } = useContext(DataContext);
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
   const { id } = useParams();
+  const history = useHistory();
   const post = posts.find((post) => post.id.toString() === id);
 
   useEffect(() => {
@@ -18,6 +18,24 @@ const EditPost = ({
       setEditBody(post.body);
     }
   }, [post, setEditBody, setEditTitle]);
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map(({ ...post }) =>
+          post.id === id ? { ...response.data } : post
+        )
+      );
+      setEditTitle("");
+      setEditBody("");
+      history.push("/");
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
 
   return (
     <main className="NewPost">
